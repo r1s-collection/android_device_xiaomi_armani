70f3a25731f6f89227f4e067a28a851d17d1107e
Deepak Sharma <techexhibeo@gmail.com>
2015-05-17 17:49:31 +0530

    fix typo :P

--- glade.mk
+++ glade.mk
@@ -14,4 +14,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
-    vendor_glade/prebuilts/common/bootanimation/720.zip:system/media/bootanimation.zip
+    vendor/glade/prebuilts/common/bootanimation/720.zip:system/media/bootanimation.zip

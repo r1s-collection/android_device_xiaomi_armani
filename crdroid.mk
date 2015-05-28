bad51f47751915032c73f780bffbb422b1cc1aa0
Deepak Sharma <techexhibeo@gmail.com>
2015-05-28 15:18:43 +0530

    crdroid-ify

--- crdroid.mk
+++ crdroid.mk
@@ -1,9 +1,9 @@
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common crDroid stuff
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := crdroid_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

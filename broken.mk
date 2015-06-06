31f5cc116c748b88612fd40c987a5615fb065103
Deepak Sharma <techexhibeo@gmail.com>
2015-06-06 19:13:51 +0200

    broken-fy

--- broken.mk
+++ broken.mk
@@ -1,9 +1,9 @@
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Broken stuff
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := broken_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_armani-userdebug
+add_lunch_combo broken_armani-userdebug

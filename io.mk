6eaadc47f2e73274b6ee1289698f57cbfa52a830
Deepak Sharma <techexhibeo@gmail.com>
2015-06-17 20:01:00 +0530

    io-ify

--- io.mk
+++ io.mk
@@ -4,6 +4,6 @@ $(call inherit-product, vendor/io/configs/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := infinitive_armani
+PRODUCT_NAME := io_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

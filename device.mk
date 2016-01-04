41ddd7d663d6aa5f07adb86721733c9b4836bbbd
Deepak Sharma <TechExhibeo@gmail.com>
2016-01-04 21:08:09 +0530

    armani : Add ART dex2oat controls

--- device.mk
+++ device.mk
@@ -30,9 +30,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-# Art
+# ART
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-flags=--no-watch-dog
+    dalvik.vm.dex2oat-filter=interpret-only \
+    dalvik.vm.image-dex2oat-filter=speed
 
 # Audio
 PRODUCT_PACKAGES += \

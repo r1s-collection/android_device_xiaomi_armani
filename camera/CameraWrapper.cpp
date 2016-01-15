54b538af12fc23061e80341f2fcb6934df2b7c14
Deepak Sharma <TechExhibeo@gmail.com>
2016-01-15 22:11:33 +0530

    Revert "CameraWrapper: Updates in HDR mode"
    This reverts commit 4071eddbcdc46c944f67e9a475cc0364a03baaa4.

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -36,9 +36,6 @@
 static android::Mutex gCameraWrapperLock;
 static camera_module_t *gVendorModule = 0;
 
-/* Xiaomi Morpho EasyHDR */
-static char KEY_QC_MORPHO_HDR[] = "morpho-hdr";
-
 static char **fixed_set_params = NULL;
 
 static int camera_device_open(const hw_module_t *module, const char *name,
@@ -154,14 +151,10 @@ static char *camera_fixup_setparams(int id, const char *settings)
 
     /* Enable Morpho EasyHDR and disable flash in HDR mode */
     if (hdrMode && !videoMode) {
-        params.set(KEY_QC_MORPHO_HDR, "true");
-        params.set("ae-bracket-hdr", "AE-Bracket");
-        params.set("capture-burst-exposures", "-6,8,0");
+        params.set("morpho-hdr", "true");
         params.set(android::CameraParameters::KEY_FLASH_MODE, android::CameraParameters::FLASH_MODE_OFF);
     } else {
-        params.set(KEY_QC_MORPHO_HDR, "false");
-        params.set("ae-bracket-hdr", "Off");
-        params.set("capture-burst-exposures", "0,0,0");
+        params.set("morpho-hdr", "false");
     }
 
 #if !LOG_NDEBUG
